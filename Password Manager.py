71e4637c74a795182269d83dc9a0ab7ec4dab668
Nacho Ramos <ignacio.nahuel.ramos@gmail.com>
2022-04-04 17:33:26 -0300

    first commit

--- Password Manager.py
+++ Password Manager.py
@@ -6,7 +6,6 @@ import base64
 from pymongo import MongoClient
 import random
 import string
-import hvac
 import click
 import requests
 
@@ -15,52 +14,67 @@ cluster = MongoClient('mongodb+srv://IgnacioRamos:TZxa68aDGrWWVUeq@cluster0.tzlx
 db = cluster['password_manager']
 collection = db['accounts']
 
-mainpw = 'E2d25dSpas5NNHNE7fq5NCZSF6RTadtk'
+main_password = 'E2d25dSpas5NNHNE7fq5NCZSF6RTadtk'
 
 
 def sign_up():
-    mainpw = click.prompt('Ingrese la nueva password (debe tener 32 caracteres)')
 
-    while len(mainpw) != 32:
-        mainpw = click.prompt('Ingrese la nueva password (debe tener 32 caracteres)')
+    main_password = click.prompt(
+        'New password (must contain 32 characters)'
+        )
 
-    mainpw_2 = click.prompt('Ingrese nuevamente la password')
+    while len(main_password) != 32:
+        main_password = click.prompt(
+            'Re-enter new passowrd (must contain 32 characters)'
+            )
 
-    if mainpw == mainpw_2:
-        url ='https://18.231.120.197:8200/v1/secret/data/password_manager'
+    main_password_2 = click.prompt('Confirm password')
+
+    if main_password == main_password_2:
+        url = 'https://18.231.120.197:8200/v1/secret/data/password_manager'
         headers = {'X-Vault-Token': 'hvs.2mYiopcfyjBbvdbiMFmaxt9H'}
-        info = '{"data":{"user": "%s"}}' % (mainpw)
+        info = '{"data":{"user": "%s"}}' % (main_password)
 
         r = requests.post(url, headers=headers, data=info, verify=False)
 
         print(r.json())
-        print('La password fue seteada correctamente')
+        print('The new password was set correctly')
 
         log_in()
 
     else:
-        print('Las password no coinciden')
-        sign_up()
+        print("Passwords don't match")
+        return 'error'
 
 
 def log_in():
-    # ingresar main password para poder acceder al manager
-    try_1 = click.prompt('Ingrese la main password para poder acceder')
+    # Ingresar main password para poder acceder al manager
+    log_in_password = click.prompt(
+        'Ingrese la main password para poder acceder'
+        )
 
-    url ='https://18.231.120.197:8200/v1/secret/data/password_manager'
+    url = 'https://18.231.120.197:8200/v1/secret/data/password_manager'
     headers = {'X-Vault-Token': 'hvs.2mYiopcfyjBbvdbiMFmaxt9H'}
     r = requests.get(url, headers=headers, verify=False)
     print(r.json())
-    if r.json()['data']['data']['user'] == try_1:
+    if r.json()['data']['data']['user'] == log_in_password:
         print('Welcome!')
         menu()
     else:
         print('El usuario o la password es incorrecto')
-        log_in()
+        return 'error'
 
 
 def menu():
-    options = ['Seleccionar servicio', 'Agregar cuenta nueva', 'Generador de password', 'Buscar un usuario', 'Lista completa de cuentas', 'Modificar una cuenta', 'Eliminar una cuenta']
+    options = [
+        'Seleccionar servicio',
+        'Agregar cuenta nueva',
+        'Generador de password',
+        'Buscar un usuario',
+        'Lista completa de cuentas',
+        'Modificar una cuenta',
+        'Eliminar una cuenta'
+        ]
     questions = [
         {
             'type': 'list',
@@ -70,7 +84,6 @@ def menu():
         }
     ]
     answers = prompt(questions, style=custom_style_2)
-
     if answers['theme'] == 'Seleccionar servicio':
         seleccionar_servicio()
     elif answers['theme'] == 'Agregar cuenta nueva':
@@ -90,12 +103,14 @@ def menu():
 
 
 def seleccionar_servicio():
-    busqueda = click.prompt('Ingrese el servicio que desea buscar').capitalize()
+    busqueda = click.prompt(
+        'Ingrese el servicio que desea buscar'
+        ).capitalize()
 
     resultado = list(collection.find({'servicio': busqueda}))
 
     if len(resultado) == 0:
-        # no existen cuentas para ese servicio
+        # No existen cuentas para ese servicio
         print('No existen cuentas para ese servicio')
     else:
         for account in resultado:
@@ -106,7 +121,7 @@ def seleccionar_servicio():
 
 
 def hashing(password):
-    key = base64.urlsafe_b64encode(mainpw.encode('utf-8'))
+    key = base64.urlsafe_b64encode(main_password.encode('utf-8'))
     f = Fernet(key)
     password = password.encode()
     hashed = f.encrypt(password)
@@ -114,7 +129,7 @@ def hashing(password):
 
 
 def decrypt(password):
-    key = base64.urlsafe_b64encode(mainpw.encode('utf-8'))
+    key = base64.urlsafe_b64encode(main_password.encode('utf-8'))
     f = Fernet(key)
     unhashed = (f.decrypt(password)).decode('utf-8')
     return unhashed
@@ -136,7 +151,13 @@ def agregar_cuenta():
         password = click.prompt('Ingrese la password')
         hashed = hashing(password)
 
-        collection.insert_one({'servicio': servicio, 'username': username, 'password': hashed})
+        collection.insert_one(
+            {
+                'servicio': servicio,
+                'username': username,
+                'password': hashed
+                }
+                )
 
 
 def random_pasword():
@@ -199,34 +220,68 @@ def complete_list():
 
 
 def modify():
-    servicio = click.prompt('Ingrese el servicio que desea modificar').capitalize()
+    servicio = click.prompt(
+        'Ingrese el servicio que desea modificar'
+        ).capitalize()
     services = list(collection.find({'servicio': servicio}))
 
-    if len(services)== 0:
+    if len(services) == 0:
         print(f'No se encuentra el servicio: {servicio} en la base de datos')
     else:
-        eleccion = click.prompt('1. Desea modificar el usuario? \n2. Desea modificar la password? \nEleccion', type=int)
-        if eleccion == 1:
+        options = [
+                'Desea modificar el usuario?',
+                'Desea modificar la password?'
+                ]
+        questions = [
+                        {
+                            'type': 'list',
+                            'name': 'theme',
+                            'message': 'What do you want to do?',
+                            'choices': options
+                        }
+                    ]
+        answers = prompt(questions, style=custom_style_2)
+        
+        if answers['theme'] == 'Desea modificar el usuario?':
             old_user = click.prompt('Ingrese el usuario que desea modificar')
 
-            accounts = list(collection.find({'servicio':servicio, 'username':old_user}))
+            accounts = list(collection.find(
+                {
+                    'servicio': servicio,
+                    'username': old_user
+                }))
 
             if len(accounts) == 0:
-                print(f'No se encontro el usuario: {old_user} en el servicio: {servicio}')
+                print(
+                        f'No se encontro el usuario: {old_user} en el servicio: {servicio}'
+                    )
             else:
                 new_user = click.prompt('Ingrese el nuevo nombre de usuario')
 
                 if click.confirm('Esta seguro que desea modificar el usuario?'):
 
-                    collection.find_one_and_update({'servicio': servicio, 'username': old_user}, {'$set': {'username': new_user}})
+                    collection.find_one_and_update(
+                        {
+                            'servicio': servicio,
+                            'username': old_user
+                        },
+                        {
+                            '$set': {'username': new_user}
+                        })
                     print('El usuario se actualizo correctamente')
 
                 else:
                     print('Se ha cancelado la operacion')
 
-        elif eleccion == 2:
-            user = click.prompt('Ingrese el usuario que desea cambiarle la password')
-            check = list(collection.find({'servicio': servicio, 'username': user}))
+        elif answers['theme'] == 'Desea modificar la password?':
+            user = click.prompt(
+                'Ingrese el usuario que desea cambiarle la password'
+                )
+            check = list(collection.find(
+                {
+                    'servicio': servicio,
+                    'username': user
+                }))
 
             if len(check) == 0:
                 print(f'No se encontro el usuario: {user} en el servicio: {servicio}')
@@ -236,7 +291,14 @@ def modify():
                 if click.confirm('Esta seguro que desea modificar la password?'):
                     hashed = hashing(new_password)
 
-                    collection.find_one_and_update({'servicio': servicio, 'username': user}, {'$set': {'password': hashed}})
+                    collection.find_one_and_update(
+                        {
+                            'servicio': servicio,
+                            'username': user
+                        },
+                        {
+                            '$set': {'password': hashed}
+                        })
                     print('La password se ha actualizado correctamente')
 
                 else:
@@ -255,18 +317,42 @@ def delete():
         print(f'No se encuentra el servicio: {servicio} en la base de datos')
     else:
         user = click.prompt('Ingrese el usuario que desea eliminar')
-        accounts = list(collection.find({'servicio': servicio, 'username': user}))
+        accounts = list(collection.find(
+            {
+                'servicio': servicio,
+                'username': user
+            }))
 
         if len(accounts) == 0:
             print(f'No existe ningun usuario con el nombre: {user} en el servicio: {servicio}')
 
         else:
             if click.confirm('Se encontro la cuenta! \nEsta seguro que desea eliminarla?'):
-                collection.find_one_and_delete({'servicio': servicio, 'username': user})
+                collection.find_one_and_delete(
+                    {
+                        'servicio': servicio,
+                        'username': user
+                    })
                 print('Se ha eliminado la cuenta correctamente')
             else:
                 print('Se ha cancelado la operacion')
 
 
-log_in()
+# Si el tipo tiene ya una cuenta:
+# log_in()
+    # error = log_in()
+    # if error = 'error':
+        # lo mando de vuelta a la pagina principal
+    # else:
+    #   menu()
 
+# Si no tiene una cuenta:
+# sing_up()
+
+    # error = sign_up()
+    # if error = 'error':
+        # lo mando de vuelta a la pagina principal
+    # else:
+    #   log_in()
+
+menu()
