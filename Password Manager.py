5a296efd1d918a7e9b432098cc66848aea0c5161
Nacho Ramos <ignacio.nahuel.ramos@gmail.com>
2022-04-04 01:23:52 -0300

    first commit

--- Password Manager.py
+++ Password Manager.py
@@ -10,6 +10,7 @@ import hvac
 import click
 import requests
 
+HOLA LO CAMBIE AL ARCHIVO
 
 cluster = MongoClient('mongodb+srv://IgnacioRamos:TZxa68aDGrWWVUeq@cluster0.tzlxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = cluster['password_manager']
